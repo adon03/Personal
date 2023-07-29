896f4323bf61b30ef2e4a088129eef6a4b094d97
adon03 <140881229+adon03@users.noreply.github.com>
2023-07-29 15:33:50 +0700

    Add files via upload

---
+++ vsga/p.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Profil</title>
+</head>
+<body>
+    
+<center>
+        <img src="profil.jpeg" width="200" height="200" style="border-radius: 50%;">
+        <h1>Achmad Romadon</h1>
+        <p>(Web Developer)</p>
+    
+
+    <hr>
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Hi, saya adalah web developer yang berdomisisli di Sambas.
+            Saat ini sedang belajar Dasar Pemrograman Web
+    </p></article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Beginner)</li>
+                            <li>CSS (Beginner)</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Membuat Website</li>
+                            <li>Basic Things</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright © 2020 Achmad Romadon.</p>
+    </footer>
+</center>
+    
+</body>
+</html>
